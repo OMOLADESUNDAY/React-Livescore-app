7ea8693a062824c0accba173141e70e58275660f
omolade sunday <sundayomoladee11@gmail.com>
2022-10-12 22:22:51 +0100

    getting data from api

--- src/component/Scores.js
+++ src/component/Scores.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Score from './Score';
+import './Scores.css'
 
 
 
@@ -17,176 +18,6 @@ const Scores = () => {
 };
 
 
-
-
-
-// Ccd
-// : 
-// "korea-republic"
-// Ccdiso
-// : 
-// "KOR"
-// Chi
-// : 
-// 0
-// Cid
-// : 
-// "111"
-// Cnm
-// : 
-// "Republic of Korea"
-// Csnm
-// : 
-// "Republic of Korea"
-// Events
-// : 
-// Array(1)
-// 0
-// : 
-// ComX
-// : 
-// 0
-// EO
-// : 
-// 1851663
-// Eact
-// : 
-// 1
-// Ecov
-// : 
-// 0
-// Eds
-// : 
-// 20221008213016
-// Ehid
-// : 
-// 0
-// Eid
-// : 
-// "627983"
-// Epr
-// : 
-// 1
-// Eps
-// : 
-// "88'"
-// Ern
-// : 
-// 43
-// ErnInf
-// : 
-// "43"
-// Esd
-// : 
-// 20221008213000
-// Esid
-// : 
-// 3
-// Et
-// : 
-// 1
-// IncsX
-// : 
-// 1
-// LuC
-// : 
-// 0
-// LuX
-// : 
-// 0
-// Pid
-// : 
-// 8
-// Pids
-// : 
-// {1: '3817367', 6: '31803431', 8: '627983', 12: 'SBTE_27617387', 32: '2675817'}
-// SDFowX
-// : 
-// 0
-// SDInnX
-// : 
-// 0
-// Sids
-// : 
-// {1: '877725', 6: '63331', 8: '9298', 12: 'SBTC3_36043', 32: '17622'}
-// Spid
-// : 
-// 1
-// StatX
-// : 
-// 1
-// Stg
-// : 
-// {Sid: '9298', Snm: 'K-League 2', Sds: 'K-League 2', Scd: 'k-league-2', Cid: '111', …}
-// SubsX
-// : 
-// 0
-// T1
-// : 
-// [{…}]
-// T2
-// : 
-// [{…}]
-// Tr1
-// : 
-// "4"
-// Tr1OR
-// : 
-// "4"
-// Tr2
-// : 
-// "0"
-// Tr2OR
-// : 
-// "0"
-// Trh1
-// : 
-// "1"
-// Trh2
-// : 
-// "0"
-// [[Prototype]]
-// : 
-// Object
-// length
-// : 
-// 1
-// [[Prototype]]
-// : 
-// Array(0)
-// Scd
-// : 
-// "k-league-2"
-// Scu
-// : 
-// 0
-// Sdn
-// : 
-// "K-League 2"
-// Sds
-// : 
-// "K-League 2"
-// Shi
-// : 
-// 0
-// Sid
-// : 
-// "9298"
-// Snm
-// : 
-// "K-League 2"
-
-
-
-
-
-
-// fetch('https://livescore6.p.rapidapi.com/matches/v2/list-live?Category=soccer&Timezone=-7', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-
     const [tour, setTour]=useState([])
     const [event, setEvent]=useState([])
     const getTour=async()=>{
@@ -199,16 +30,58 @@ const Scores = () => {
         getTour()
          // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
+    const gameClickHandler=(id)=>{
+      console.log(id)
+          const GametoDissplay=tour.filter((person) => person.id === id)
+    }
   return (
     <section className="container">
         {tour.map((games)=>{
           return(
             <div key={games.Sid}>
-              {games.Ccd}
-              <br />
-              {games.Scd}
-              <br />
-              <Score eevent={games.Events}/>
+              <h2>{games.Ccd}</h2>
+             
+              <h2>{games.Scd}</h2>
+             
+              {games.Events.map((gameEvent)=>{
+                return(
+                  <div className='gmmm' key={gameEvent.Eid} onClick={()=>gameClickHandler(gameEvent.Eid)}>
+                      <h5 className='time'>{gameEvent.Eps==='NS'?gameEvent.Eps==='? ?':gameEvent.Eps}</h5>
+                      <div className='ghff'>
+
+                      <div className='team1'>
+                          {gameEvent.T1.map((team)=>{
+                      return(
+                        <div key={team.ID}>
+                            <p>{team.Nm}</p>
+                        </div>
+                      )
+                      })}
+
+                      <h1>{gameEvent.Tr1}</h1>
+                      </div>
+                      <div className='team2'>
+                          <h1>{gameEvent.Tr2}</h1>
+                      {gameEvent.T2.map((team2)=>{
+                        return(
+                        <div key={team2.ID}>
+                          <p>{team2.Nm}</p>
+                          
+                        </div>
+                        
+                       )
+                   })}
+                      </div>
+
+                      
+                      </div>
+                      <p>Star</p>
+                      
+                      </div>
+                  
+                )
+                
+              })}
             </div>
 
             
